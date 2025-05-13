faf6a997c456c320600e76fe68cea9e2eaf90981
Andrea-RC7 <323030024@upjr.edu.mx>
2025-05-13 08:56:30 -0600

    Update martes13mayo.ts

--- martes13mayo.ts
+++ martes13mayo.ts
@@ -1,1 +1,16 @@
-codigo
+(()=>{
+const edad=[35,50,40,18,30];
+const nombres = ['Hugo','Paco','Luis','Pedro','Hector'];
+
+const nombresCompletos:string[] = nombres.map((nombre, index) => {
+  return `${nombre} ${edad[index]}`;
+});
+
+const min= edad.indexOf(Math.min(...edad));
+const max= edad.indexOf(Math.max(...edad));
+
+console.log('El menor es:',nombresCompletos[min], 'años'); 
+console.log('El mayor es:',nombresCompletos[max], 'años'); 
+
+
+})()
