44c861e90b37abc6326103bf488a7a075261bc0d
Andrea-RC7 <323030024@upjr.edu.mx>
2025-05-20 08:45:57 -0600

    Update p1.ts

--- problemas/p1.ts
+++ problemas/p1.ts
@@ -1,1 +1,15 @@
+(()=>{
+    
+ const addTwoNumbers = (a:number, b:number):number => {
+  return a + b;
+};
 
+const hola=  () => {
+    
+    console.log(addTwoNumbers(2, 4)===6);
+    console.log(addTwoNumbers(10, 10)===20);
+};
+
+hola();
+
+})()
