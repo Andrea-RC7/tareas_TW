c04a8c7b1deb09b10239a03bcbe25a2395090683
Andrea-RC7 <323030024@upjr.edu.mx>
2025-05-28 08:01:27 -0600

    Update genericos.ts

--- genericos/genericos.ts
+++ genericos/genericos.ts
@@ -1,3 +1,18 @@
+//ejemplo 1
+
 export function reverseArray<U>(items: U[]): U[] {
   return items.reverse();
 }
+
+import { funcionGenerica, funcionGenericaFlecha, reverseArray } from "./genericos/genericos";
+console.log(reverseArray(["one", "two", "three"]))
+
+
+//ejemplo 2
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
+
+// Uso
+const person = { name: "Ana", age: 25 };
+const personName = getProperty(person, "name");
